45ad28f3124ed2172219130cce3bb1202b2a282c
yuriagile <agiledev22@gmail.com>
2022-09-12 10:18:04 +0300

    remove honey field from deposit instr

--- src/wrappers/user.ts
+++ src/wrappers/user.ts
@@ -640,7 +640,6 @@ export class HoneyUser implements User {
           vault: reserve.data.vault,
           depositNoteMint: reserve.data.depositNoteMint,
 
-          honeyProgram: this.client.program.programId,
           tokenProgram: TOKEN_PROGRAM_ID,
         }).instruction()
     );
