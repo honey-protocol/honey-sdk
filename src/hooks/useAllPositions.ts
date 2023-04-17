f6dec128331fd7f232bd932466671f3679b8f2f8
Bowtiedpyro <bowtiedpyro@gmail.com>
2023-04-17 13:49:15 +0200

    update api urls

--- src/hooks/useAllPositions.ts
+++ src/hooks/useAllPositions.ts
@@ -45,8 +45,8 @@ export const useAllPositions = (
     console.log('fetching bids...');
     const resBids = await fetch(
       isDevnet
-        ? `https://honey-nft-api.herokuapp.com/bids/${honeyMarketId}`
-        : `https://honey-mainnet-api.herokuapp.com/bids/${honeyMarketId}`,
+        ? `https://honeyfinance.xyz/bids/${honeyMarketId}`
+        : `https://honeyfinance.xyz/bids/${honeyMarketId}`,
       { mode: 'cors' },
     );
 
