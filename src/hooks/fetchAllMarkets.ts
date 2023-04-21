1984d53dd55616f5669f7a3f415129dc2aac76a2
Bowtiedpyro <bowtiedpyro@gmail.com>
2023-04-21 12:08:16 +0200

    honey user array | update API urls

--- src/hooks/fetchAllMarkets.ts
+++ src/hooks/fetchAllMarkets.ts
@@ -134,8 +134,8 @@ const fetchPositionsAndBids = async (
   console.log('fetching bids...');
   const resBids = await fetch(
     isDevnet
-      ? `https://honey-nft-api.herokuapp.com/bids/${honeyMarketId}`
-      : `https://honey-mainnet-api.herokuapp.com/bids/${honeyMarketId}`,
+      ? `https://honeyfinance.xyz/bids/${honeyMarketId}`
+      : `https://honeyfinance.xyz/bids/${honeyMarketId}`,
     { mode: 'cors' },
   );
   const arrBids = await resBids.json();
--- src/hooks/useMarket.ts
+++ src/hooks/useMarket.ts
@@ -11,60 +11,79 @@ import { ConnectedWallet } from '../helpers/walletType';
  * @param connection to the cluster
  * @param wallet wallet adapter or null for read-only
  * @param honeyProgramId of the program
- * @param honeyMarketId single market id to fetch
- * @returns HoneyClient, HoneyMarket, HoneyUser, HoneyReserve[] and their setters
+ * @param honeyMarketId array of market ids
+ * @returns array of HoneyUsers
  */
 export const useMarket = (
   connection: Connection,
-  wallet: ConnectedWallet | null,
+  // remove '| null'  - since we want to use this func. to init a honey user for transactions 
+  wallet: ConnectedWallet,
   honeyProgramId: string,
-  honeyMarketId: string,
+  honeyMarketId: string[],
 ) => {
   const { isConfigured } = useAnchor();
 
   const [honeyClient, setHoneyClient] = useState<HoneyClient>();
   const [honeyMarket, setHoneyMarket] = useState<HoneyMarket>();
-  const [honeyUser, setHoneyUser] = useState<HoneyUser>();
+  const [honeyUser, setHoneyUser] = useState<HoneyUser[]>([]);
   const [honeyReserves, setHoneyReserves] = useState<HoneyReserve[]>();
+  let honeyUserArray = [];
 
   useEffect(() => {
-    const readOnlyKeypair = new Keypair();
-    const fetchHoneyClient = async () => {
+    const fetchHoneyClient = async (marketId: string) => {
+      // init provider 
       const provider = new anchor.AnchorProvider(
         connection,
-        wallet ?? new NodeWallet(readOnlyKeypair),
+        wallet,
         anchor.AnchorProvider.defaultOptions(),
       );
+      // init and set client
       const client: HoneyClient = await HoneyClient.connect(provider, honeyProgramId, true);
       setHoneyClient(client);
-
-      const honeyMarketPubKey: PublicKey = new PublicKey(honeyMarketId);
+      // init and set market
+      const honeyMarketPubKey: PublicKey = new PublicKey(marketId);
       const market: HoneyMarket = await HoneyMarket.load(client, honeyMarketPubKey);
       setHoneyMarket(market);
 
-      // pull latest reserve data
-      market.refresh();
-
+      // pull latest reserve data - add await
+      await market.refresh();
+      // init market reserves
       const reserves: HoneyReserve[] = market.cachedReserveInfo.map(
         (reserve) => new HoneyReserve(client, market, reserve.reserve),
       );
+      // refresh all reserves so we have the latest data - set the reserves
       await Promise.all(
         reserves.map(async (reserve) => {
           if (reserve.reserve && reserve.reserve.toBase58() !== PublicKey.default.toBase58()) await reserve.refresh();
         }),
       );
+      
       setHoneyReserves(reserves);
-
+      // init and set the honey user
       const user: HoneyUser = await HoneyUser.load(
         client,
         market,
-        wallet ? new PublicKey(wallet.publicKey) : readOnlyKeypair.publicKey,
+        new PublicKey(wallet.publicKey),
         reserves,
       );
-      setHoneyUser(user);
+      // setHoneyUser(user);
+      honeyUserArray.push(user);
     };
-    if (isConfigured && connection && honeyProgramId && honeyMarketId) {
-      fetchHoneyClient();
+
+    const runMarkets = async () => {
+      await Promise.all(
+        honeyMarketId.map((marketId: string) => {
+          fetchHoneyClient(marketId);
+        })
+      ).then((res) => {
+        setHoneyUser(honeyUserArray);
+      }).catch((err) => {
+        console.log(`Error occurred running fetchHoneyClient: ${err}`);
+      })
+    }
+
+    if (isConfigured && connection && honeyProgramId && honeyMarketId.length) {
+      runMarkets();
     }
   }, [isConfigured, connection, honeyProgramId, honeyMarketId]);
 
