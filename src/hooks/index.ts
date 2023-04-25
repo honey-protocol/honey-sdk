0b5e08672677d04fc336cf82f87a3d861cec836c
bowtiedpyro <bowtiedpyro@gmail.com>
2023-04-25 21:35:02 +0200

    remove useBorrowPositions

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,5 @@
-// export * from './useBorrowPositions';
 export * from './useMarket';
 
-// export { useBorrowPositions } from './useBorrowPositions';
 export { useMarket } from './useMarket';
 export { useAllPositions } from './useAllPositions';
 export { fetchAllMarkets, MarketBundle, fetchPositionsAndBids } from './fetchAllMarkets';
