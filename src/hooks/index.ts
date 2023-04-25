7ce5283bec81ba477b4cb22082ca3ff4f7ef13e0
furyeyes <ioannis.aarde@gmail.com>
2023-04-25 21:21:24 +0200

    outcomment useBorrowPositions | create array for useMarket in honey.tsx file

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,7 @@
-export * from './useBorrowPositions';
+// export * from './useBorrowPositions';
 export * from './useMarket';
 
-export { useBorrowPositions } from './useBorrowPositions';
+// export { useBorrowPositions } from './useBorrowPositions';
 export { useMarket } from './useMarket';
 export { useAllPositions } from './useAllPositions';
 export { fetchAllMarkets, MarketBundle, fetchPositionsAndBids } from './fetchAllMarkets';
